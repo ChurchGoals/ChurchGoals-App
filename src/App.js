6f5dfc35fd5e134c9b7d5fe2076936517ceb9033
Mike Hoffert <mike.hoffert@gmail.com>
2019-12-31 08:42:41 -0700

    Use an image background for landing page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Linking, SafeAreaView, Text, View } from 'react-native';
+import { Image, Linking, ScrollView, Text, View } from 'react-native';
 import Button from './components/button';
 import Styles from './App.style';
+import carpentersSon from '../assets/images/carpenters-son.jpg';
 
 function App() {
   return (
-    <View style={Styles.container}>
-      <SafeAreaView>
-        <Text style={Styles.title}>Church</Text>
-        <Text style={Styles.title}>Goals</Text>
-        <Button>Login</Button>
-        <Text>OR</Text>
-        <Button type="secondary">Signup</Button>
-        <Text style={Styles.notChurch}>This is NOT a church run application.</Text>
-        <Text style={Styles.churchLink} onPress={() => Linking.openURL('https://www.churchofjesuschrist.org/youth/childrenandyouth')}>Find out more about the Church of Jesus Christ of Latter-day Saints Personal Development Program</Text>
-      </SafeAreaView>
-    </View>
+    <ScrollView style={Styles.container}>
+      <View style={Styles.imageContainer}>
+        <Image source={carpentersSon} style={Styles.image}/>
+        <View style={Styles.titleContainer}>
+          <Text style={Styles.title}>Church</Text>
+          <Text style={Styles.title}>Goals</Text>
+        </View>
+      </View>
+      <Button>Login</Button>
+      <Text style={Styles.or}>OR</Text>
+      <Button type="secondary">Signup</Button>
+      <Text style={Styles.notChurch}>This is NOT a church run application.</Text>
+      <Text style={Styles.churchLink} onPress={() => Linking.openURL('https://www.churchofjesuschrist.org/youth/childrenandyouth')}>Find out more about the Church of Jesus Christ of Latter-day Saints Personal Development Program</Text>
+    </ScrollView>
   );
 }
 
--- src/App.style.js
+++ src/App.style.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+import Color from '@alchemyalcove/color-calculator';
 import colors from './colors';
 
 export default StyleSheet.create({
@@ -7,11 +8,34 @@ export default StyleSheet.create({
     height: '100%',
   },
 
+  imageContainer: {
+    marginBottom: 50,
+  },
+
+  image: {
+    aspectRatio: 1,
+    height: 400,
+    width: '100%',
+  },
+
+  titleContainer: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    paddingBottom: 20,
+  },
+
   title: {
-    color: colors.black,
+    color: new Color(colors.black).darken(10).toHex(),
     fontSize: 100,
     fontFamily: 'KulimPark-Light',
     height: 100,
+    textAlign: 'center',
+  },
+
+  or: {
+    marginVertical: 18,
     textAlign: 'center',
   },
 
@@ -27,6 +51,7 @@ export default StyleSheet.create({
     fontFamily: 'InriaSerif-Regular',
     color: '#0000FF',
     paddingHorizontal: 40,
+    marginBottom: 50,
     textAlign: 'center',
   },
 });
